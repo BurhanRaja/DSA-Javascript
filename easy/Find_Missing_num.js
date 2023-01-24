0267bf1ae9f7ce54dd5a562c04717d34ce397330
BurhanuddinRaja <mails4burhan@gmail.com>
2023-01-24 11:34:43 +0530

    find missing num modified

--- easy/Find_Missing_num.js
+++ easy/Find_Missing_num.js
@@ -1,25 +1,13 @@
 // let n = [1,2,3,5];
 let n = [6,1,2,8,3,4,7,10,5];
 
-function findMissing(array) {
-    let i = 0;
-    while (i < array.length) {
-        let correct = array[i] - 1;
-        if (!array.includes(correct) && correct !== 0 ) {
-            return correct;
-        }
-        if (array[i]<array.length && array[i] !== array[correct]) {
-            swap(array, i, correct);
-        } else {
-            i++;
+// Easy
+function findMissing(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr.includes(i+1)) {
+            return i+1;
         }
     }
-}
-
-function swap(arr, a, b) {
-    let temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
 }
 
 console.log(findMissing(n));
