1e8a1a8450d7453cde733b7bdc471f5bfffae4d5
BurhanuddinRaja <mails4burhan@gmail.com>
2023-01-27 16:17:46 +0530

    one modified

--- easy/Count_Distinct_element_in_window.js
+++ easy/Count_Distinct_element_in_window.js
@@ -1,7 +1,7 @@
-// let arr = [1, 2, 1, 3, 4, 2, 3];
-// let k = 4;
-let arr = [4,1,1];
-let k = 2;
+let arr = [1, 2, 1, 3, 4, 2, 3];
+let k = 4;
+// let arr = [4, 1, 1];
+// let k = 2;
 
 function countEl(arr, k) {
   let output = [];
@@ -15,4 +15,39 @@ function countEl(arr, k) {
   console.log(output);
 }
 
-countEl(arr, k);
+function countEl2(arr, k) {
+  let output = [];
+  let hMap = new Map();
+
+  for (let i = 0; i < k; i++) {
+    if (hMap.has(arr[i])) {
+      hMap.set(arr[i], hMap.get(arr[i]) + 1);
+    } else {
+      hMap.set(arr[i], 1);
+    }
+  }
+
+  output.push(hMap.size);
+
+  for (let i = k; i < arr.length; i++) {
+    if (hMap.get(arr[i - k]) === 1) {
+      hMap.delete(arr[i - k]);
+    } else {
+      hMap.set(arr[i - k], hMap.get(arr[i - k] - 1));
+    }
+
+    if (hMap.has(arr[i])) {
+      hMap.set(arr[i], hMap.get(arr[i]) + 1)
+    }
+    else {
+      hMap.set(arr[i], 1)
+    };
+
+    output.push(hMap.size);
+  }
+
+  console.log(output);
+}
+
+// countEl(arr, k);
+countEl2(arr, k);
