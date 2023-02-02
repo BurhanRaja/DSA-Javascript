ee38b6cd96db656131061f6a6ccda1c974a8d97f
BurhanuddinRaja <mails4burhan@gmail.com>
2023-02-02 12:36:54 +0530

    array pair sum divisibiility problem

---
+++ easy/Array_Pair_Sum_Divisibility_Problem.js
@@ -0,0 +1,16 @@
+let arr = [9, 5, 7, 3], k = 6;
+
+function sumDivisibility(arr, k) {
+    let flag = true;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            if ((arr[i] + arr[j])%k === 0) {
+                flag = true;
+            }
+        }
+    }
+
+    return flag ? true : false;
+}
+
+sumDivisibility(arr, k);
