dcabce06b59084aa119fc1ebecde63938411849a
BurhanuddinRaja <mails4burhan@gmail.com>
2023-02-07 12:03:40 +0530

    notes added

---
+++ Notes.js
@@ -0,0 +1,5 @@
+// BST - Binary Search Tree is a tree which contains small value on left and greater value on right
+// Tree - is a simple tree no exception
+// Heap - is a tree in a sorted form .Min-heap is root should be minimum and as it goes down the values increases ans Max-heap is root should be maximum and as it goes down the values decreases.
+
+// Graph - It contains udirected or directed graph. They are inter-connected.
